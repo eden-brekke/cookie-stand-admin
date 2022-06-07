9e91500dd59b1282c4ac0392d0954250a5c6d865
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-06-07 13:07:36 -0700

    added default tailwind content from docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = { 
+	content: [ 
+		"./pages/**/*.{js,ts,jsx,tsx}", 
+		"./components/**/*.{js,ts,jsx,tsx}", 
+  ], 
+  theme: { 
+    extend: {
+
+    }, 
+  }, 
+  plugins: [
+
+  ], 
 }
